1901af75bbcd3bc4c9ede323e23bb363061d10b9
Brad Williams <bwillia2@andrew.cmu.edu>
2023-02-12 16:30:13 -0500

    setup frontend-backend prototype. web page should display 'glazed donut' string from back-end. CORS middleware was addded to make this work

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState<{ id: number, name: string }[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:80/')
+      .then(response => {
+        console.log('response: ', response);
+        return response.json();
+      })
+      .then(data => {
+        console.log('data: ', data);
+        setData(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +24,14 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          Data from the backend:
+        </p>
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import express from "express";
+import cors from "cors";
 
-const app = express()
-const host = '0.0.0.0'
-const port = 80
+const app = express();
+const host = '0.0.0.0';
+const port = 80;
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  const data = [
+    { id: 1, name: 'glazed donut' },
+  ];
+
+  res.json(data);
+});
 
 app.listen(port, host, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+export { app };
