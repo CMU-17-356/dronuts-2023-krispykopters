382e4cc9652392cad981bfb92bc08c30888ffdca
Brad Williams <bwillia2@andrew.cmu.edu>
2023-02-28 12:22:46 -0500

    changed button upon completing order

--- src/components/Order/index.tsx
+++ src/components/Order/index.tsx
@@ -1,52 +1,63 @@
+import { useState } from "react";
 import type { Order } from "../../../types";
 import { fulfillOrder } from "../../utils/functions";
 import { motion } from "framer-motion";
 
 export const OrderDisplay = ({
-    order,
-    col,
-    admin,
-  }: {
-    order: Order;
-    col?: boolean;
-    admin?: boolean;
-  }) => {
-    return (
-        <motion.div
-        whileTap={{ rotate: [0, -1, 1, -1, 0] }}
-        className={`${
+  order,
+  col,
+  admin,
+}: {
+  order: Order;
+  col?: boolean;
+  admin?: boolean;
+}) => {
+  const [isFulfilled, setIsFulfilled] = useState(false);
+
+  const handleFulfillOrder = () => {
+    // fulfillOrder(order._id);
+    setIsFulfilled(true);
+  };
+
+  return (
+    <motion.div
+      whileTap={{ rotate: [0, -1, 1, -1, 0] }}
+      className={`${
         !col ? "w-[275px] min-w-[275px]" : "w-[320px] min-w-[320px]"
       } md:w-[300px] md:min-w-[300px] ${
         col ? "my-12" : "my-2 md:my-5"
       } h-auto bg-cardOverlay rounded-lg p-2 px-3 backdrop-blur-lg hover:drop-shadow-sm cursor-pointer`}
     >
-        <div>
-            <p>Order #{order._id}</p>
-            <table>
-                <thead className = "flex items-end">
-                    <th>Items</th>
-                </thead>
-                <tbody>
-                    {order.donuts && order.donuts.map(donut =>
-                        <tr>
-                            <td>{donut.title}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-            <motion.div
-                whileHover={{ scale: 1.2 }}
-                whileTap={{ scale: 1.1 }}
-            >
-                <button
-                className="flex items-center gap-2 justify-center bg-green-200 padding p-2 p-2"
-                onClick={()=>fulfillOrder}>Load to Drone {order.drone}
-                </button>
-
-            </motion.div>
-        </div>
+      <div>
+        <p>Order #{order._id}</p>
+        <table>
+          <thead>
+            <th>Items</th>
+          </thead>
+          <tbody>
+            {order.donuts &&
+              order.donuts.map((donut) => (
+                <tr>
+                  <td>{donut.title}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        <motion.div
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 1.1 }}
+        >
+          <button
+            className={`flex items-center gap-2 justify-center bg-${
+              isFulfilled ? "gray" : "green"
+            }-200 padding p-2 p-2`}
+            onClick={handleFulfillOrder}
+            disabled={isFulfilled}
+          >
+            {isFulfilled ? "Fulfilled" : "Complete Order"}
+          </button>
         </motion.div>
-    );
-    
+      </div>
+    </motion.div>
+  );
 };
-    
