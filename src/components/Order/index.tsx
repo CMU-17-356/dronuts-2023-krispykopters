513d08b7b3ed61c6d7b3bbfdf21801b3b9549d4b
Mahith Edula <medula@andrew.cmu.edu>
2023-02-28 11:19:08 -0500

    Added styling to the donut cards

--- src/components/Order/index.tsx
+++ src/components/Order/index.tsx
@@ -1,5 +1,6 @@
 import type { Order } from "../../../types";
 import { fulfillOrder } from "../../utils/functions";
+import { motion } from "framer-motion";
 
 export const OrderDisplay = ({
     order,
@@ -11,24 +12,40 @@ export const OrderDisplay = ({
     admin?: boolean;
   }) => {
     return (
+        <motion.div
+        whileTap={{ rotate: [0, -1, 1, -1, 0] }}
+        className={`${
+        !col ? "w-[275px] min-w-[275px]" : "w-[320px] min-w-[320px]"
+      } md:w-[300px] md:min-w-[300px] ${
+        col ? "my-12" : "my-2 md:my-5"
+      } h-auto bg-cardOverlay rounded-lg p-2 px-3 backdrop-blur-lg hover:drop-shadow-sm cursor-pointer`}
+    >
         <div>
             <p>Order #{order._id}</p>
             <table>
                 <thead>
-                    <th>Item</th>
-                    < th>Qty</th>
+                    <th>Items</th>
                 </thead>
                 <tbody>
                     {order.donuts && order.donuts.map(donut =>
                         <tr>
                             <td>{donut.title}</td>
-                            <td>{donut.qty}</td>
                         </tr>
-                    )};
+                    )}
                 </tbody>
             </table>
-            <button onClick={()=>fulfillOrder}> Complete</button>
+            <motion.div
+                whileHover={{ scale: 1.2 }}
+                whileTap={{ scale: 1.1 }}
+            >
+                <button
+                className="flex items-center gap-2 justify-center bg-green-200 padding p-2 p-2"
+                onClick={()=>fulfillOrder}>Complete Order
+                </button>
+
+            </motion.div>
         </div>
+        </motion.div>
     );
     
 };
