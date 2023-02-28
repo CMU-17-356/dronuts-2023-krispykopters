a0ddfca1f7fd7c64eb4cdac60bbec161cc7456d0
Mahith Edula <medula@andrew.cmu.edu>
2023-02-27 20:01:38 -0500

    Added Mock OrdersData

---
+++ src/utils/customersData.tsx
@@ -0,0 +1,15 @@
+import type { Customer } from "../../types";
+
+export const customer1 : Customer = {
+    _id: "1",
+    username: "user1",
+    password: "password1",
+    displayName: "John"
+};
+
+export const customer2 : Customer = {
+    _id: "2",
+    username: "user2",
+    password: "password2",
+    displayName: "Jill"
+};
