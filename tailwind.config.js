dbe4c004ff90dac8845b9c949e865e25e21c41d0
Joseph-ljx <92981525+Joseph-ljx@users.noreply.github.com>
2023-03-03 12:43:36 -0500

    remove redundant depencies: styleMock & babel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+// Tailwind is a framework for building bespoke user interfaces
+// A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 
+// that can be composed to build any design, directly in your markup.
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
