093e8e282c6f9ac9b6dd761024df30230363a81a
JunTaoLuo <jtluo@andrew.cmu.edu>
2023-03-19 15:53:14 -0400

    Update order ODM

--- server/db/order.ts
+++ server/db/order.ts
@@ -5,10 +5,12 @@ import { ICoord } from "./coord";
  * Order Interface
  */
 export interface IOrder {
-  customer : Types.ObjectId, // Reference to Customer
-  donuts : [Types.ObjectId], // List of donuts in order
-  location : ICoord,  // Delivery coordinates
-  orderTime : Date    // Time of order
+  customer : Types.ObjectId,  // Reference to Customer
+  donuts : [Types.ObjectId],  // List of donuts in order
+  location : ICoord,          // Delivery coordinates
+  status: string,             // "Placed", "OutForDelivery" or "Delivered"
+  drone?: string,             // The Id of the drone for delivery
+  orderTime : Date            // Time of order
 }
 
 /**
@@ -27,6 +29,13 @@ const OrderSchema = new Schema<IOrder>({
   location: {
     type: Schema.Types.Mixed,
     required: true
+  },
+  status: {
+    type:Schema.Types.String,
+    required: true
+  },
+  drone: {
+    type: Schema.Types.String
   },
   orderTime: {
     type: Schema.Types.Date,
