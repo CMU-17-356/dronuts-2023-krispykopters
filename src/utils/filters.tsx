c2bed317350ab4cfec110511b1077959580c7053
JunTaoLuo <jtluo@andrew.cmu.edu>
2023-03-19 22:47:05 -0400

    Handle null input

--- src/utils/filters.tsx
+++ src/utils/filters.tsx
@@ -3,6 +3,10 @@ import { Donut } from "../../types";
 import { useStateValue } from "../context/StateProvider";
 
 export const FilterFood = (donuts: Donut[], category: string) => {
+  if (donuts == null) {
+    return []
+  }
+
   return donuts.filter(
     (item: Donut) => item.category.toLowerCase() === category.toLowerCase()
   );
